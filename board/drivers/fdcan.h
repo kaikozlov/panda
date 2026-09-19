0559deb33007836f85e07f5d985cb3b3cb8c0f42
Kai Kozlov <37962720+kaikozlov@users.noreply.github.com>
2026-09-19 12:55:35 -0500

    test: keep BRS off with manual FD policy

--- board/drivers/fdcan.h
+++ board/drivers/fdcan.h
@@ -235,7 +235,7 @@ void can_rx(uint8_t can_number) {
     if (!(bus_config[can_number].canfd_enabled) && (canfd_frame)) {
       bus_config[can_number].canfd_enabled = true;
     }
-    if (!(bus_config[can_number].brs_enabled) && (brs_frame)) {
+    if (!(bus_config[can_number].brs_enabled) && (brs_frame) && bus_config[can_number].canfd_auto) {
       bus_config[can_number].brs_enabled = true;
     }
 
